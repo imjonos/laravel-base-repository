7a68858c3db5e90fc9831ba37b5c5c5f0b7504d5
Eugeny Nosenko <Imnosok@gmail.com>
2026-03-21 00:18:23 +0300

    Added insert upsert. Logic fixed

--- src/EloquentRepository.php
+++ src/EloquentRepository.php
@@ -5,6 +5,7 @@ namespace Nos\BaseRepository;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Nos\BaseRepository\Interfaces\EloquentRepositoryInterface;
 
 abstract class EloquentRepository implements EloquentRepositoryInterface
@@ -36,27 +37,49 @@ abstract class EloquentRepository implements EloquentRepositoryInterface
         return $this->getModel()->create($data);
     }
 
+    public function insert(array $data): bool
+    {
+        if (empty($data)) {
+            return false;
+        }
+
+        return $this->getModel()->newModelQuery()->insert($data);
+    }
+
+    public function upsert(array $values, array|string $uniqueBy, ?array $update = null): int
+    {
+        if (empty($values)) {
+            return 0;
+        }
+
+        return $this->getModel()->upsert($values, $uniqueBy, $update ?? []);
+    }
+
     public function update(int $id, array $data): bool
     {
-        return $this->getModel()->findOrFail($id)->update($data);
+        if (empty($data)) {
+            return false;
+        }
+
+        return $this->getModel()->where('id', $id)->update($data) > 0;
     }
 
     public function exists(int $id): bool
     {
-        return $this->getModel()->whereId($id)->exists();
+        return $this->getModel()->whereKey($id)->exists();
     }
 
     public function find(int $id): ?Model
     {
-        return $this->getModel()->findOrFail($id);
+        return $this->getModel()->find($id);
     }
 
     public function delete(int $id): bool
     {
-        return $this->getModel()->findOrFail($id)->delete();
+        return $this->getModel()->whereKey($id)->delete() > 0;
     }
 
-    public function query(): ?Builder
+    public function query(): Builder
     {
         return $this->getModel()->newQuery();
     }
